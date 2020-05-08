e405cebec1ad033c87a129850937de752bca9a8d
sukolekar <surajkolekar123@gmail.com>
2020-05-08 18:06:28 +0530

    contact form created

---
+++ css/contactform.php
@@ -0,0 +1,24 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_form = 'suraj.surajkolear@gmail.com';
+
+  $email_subject = "New form submission";
+
+  $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n". 
+                      "User Message: $message.\n";
+
+    $to = "surajkolekar123@gmail.com";
+
+    $headers = "From: $email_form \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: contact.html"); 
+
+
+?>
